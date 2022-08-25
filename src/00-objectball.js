bcf41d103817e1d2033d177ce7b54f7a54751896
dennis00095 <dennis.mwongela@student.moringaschool.com>
2022-08-25 13:03:43 +0300

    Lab Complete

---
+++ src/00-objectball.js
@@ -0,0 +1,14 @@
+function gameObject(){
+    const Object = {
+        home : {
+            teamName : '',
+            color : [],
+            players :{}
+
+
+        }
+    }
+}
+        away : {
+
+        }
